1057da16a2bfb047da98f2b739e94ef198589cdd
VastiDev <vastiane1@gmail.com>
2023-07-29 20:59:11 -0300

    interface repository user findById

---
+++ src/main/java/br/com/vastidev/api/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package br.com.vastidev.api.repositories;
+
+import br.com.vastidev.api.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+}
