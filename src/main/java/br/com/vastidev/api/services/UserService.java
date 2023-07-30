f07ea517a1a0baaeeae4f0abd27143ca74320ad7
VastiDev <vastiane1@gmail.com>
2023-07-30 18:47:35 -0300

    endpoint findAll para Users

--- src/main/java/br/com/vastidev/api/services/UserService.java
+++ src/main/java/br/com/vastidev/api/services/UserService.java
@@ -2,7 +2,10 @@ package br.com.vastidev.api.services;
 
 import br.com.vastidev.api.domain.Users;
 
+import java.util.List;
+
 public interface UserService {
     Users findById(Integer id);
 
+    List<Users> findAll();
 }
