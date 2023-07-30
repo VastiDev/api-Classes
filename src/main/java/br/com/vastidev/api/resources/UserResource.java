acac55b12fdec92eb91fbe0bf47bb4c9eeceb1ea
VastiDev <vastiane1@gmail.com>
2023-07-29 21:01:07 -0300

    classe user resource findById

--- src/main/java/br/com/vastidev/api/resources/UserResource.java
+++ src/main/java/br/com/vastidev/api/resources/UserResource.java
@@ -1,6 +1,8 @@
 package br.com.vastidev.api.resources;
 
 import br.com.vastidev.api.domain.User;
+import br.com.vastidev.api.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,10 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/user")
 public class UserResource {
+
+    @Autowired
+    private UserService service;
     @GetMapping("/{id}")
     public ResponseEntity<User> findById(@PathVariable Integer id){
-        return ResponseEntity.ok().body(new User(1, "Vastiane",
-                "vastiane1@gmail.com", "123"));
+        return ResponseEntity.ok().body(service.findById(id));
 
     }
 }
