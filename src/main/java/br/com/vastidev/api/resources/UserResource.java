75441a69d0861aaaec582bc55846ff1c096ffa02
VastiDev <vastiane1@gmail.com>
2023-07-30 18:16:35 -0300

    disponibilizando model mapper em forma de bean - padrão DTO

--- src/main/java/br/com/vastidev/api/resources/UserResource.java
+++ src/main/java/br/com/vastidev/api/resources/UserResource.java
@@ -1,7 +1,8 @@
 package br.com.vastidev.api.resources;
 
-import br.com.vastidev.api.domain.Users;
+import br.com.vastidev.api.domain.dto.UsersDto;
 import br.com.vastidev.api.services.UserService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,10 +17,13 @@ public class UserResource {
     private static final String ID = "/{id}";
 
     @Autowired
+    private ModelMapper mapper;
+
+    @Autowired
     private UserService service;
 
     @GetMapping("/{id}")
-    public ResponseEntity<Users> findById(@PathVariable Integer id){
-        return ResponseEntity.ok().body(service.findById(id));
+    public ResponseEntity<UsersDto> findById(@PathVariable Integer id){
+        return ResponseEntity.ok().body(mapper.map(service.findById(id), UsersDto.class));
     }
 }
